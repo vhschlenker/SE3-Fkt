d4e2d4864adfc3a1c267f0b40dd338ae9ea808b6
vhschlenker <vhschlenker@wtnet.de>
2016-12-19 10:42:13 +0100

    Merge in my changes

--- Blatt7.rkt
+++ Blatt7.rkt
@@ -2,17 +2,15 @@
 (require 2htdp/image)
 (require 2htdp/universe)
 ;Aufgabe 1
-(define testlist '(2 4 3))
-(define testzahl 3)
 
-;Teil 1: allgemein rekursiv:
-(define (produkt_aR list zahl)
+; allgemein Rekursiv
+(define (produkt1 list zahl)
   (if (empty? list)
       '()
-      (cons (* (car list) zahl)(produkt_aR (cdr list) zahl))))
+      (cons (* (car list) zahl)(produkt1 (cdr list) zahl))))
 
-;Teil 2: endrekursiv
-(define (produkt_eR list zahl)
+; Endrekursiv 1
+(define (produkt2 list zahl)
   (letrec ([innere (lambda(list ergebnis)
                      (if (empty? list)
                          (reverse ergebnis)
@@ -20,91 +18,172 @@
   (innere list '()))
   )
 
-;Teil 3: hoehere Ordnung:
-(define (produkt_hO list zahl)
+; Endrekursiv 2
+(define (produkt3 liste multi [result '()])
+  (if (empty? liste)
+      (reverse result)
+      (produkt3 (cdr liste) multi (cons (* multi (car liste)) result))))
+
+; Höhere Ordnung 1
+(define (produkt4 list zahl)
   (map (lambda(number)
          (* zahl number))list))
 
-;zum testen werden die drei Funktionen aufgerufen:
-(produkt_aR testlist testzahl)
-(produkt_eR testlist testzahl)
-(produkt_hO testlist testzahl)
+; Höhere Ordnung 2
+(define (produkt5 liste multi)
+  (map (curry * multi) liste))
 
-;Aufgabe 2:
-;2.1 Zunaechst legen wir folgende Nummerierung fuer die Segmente fest:
-;1. Mitte oben        2. oben links        3. oben rechts        4. Mitte
-;5. links unten       6. rechts unten      7. Mitte unten
-;Wir merken uns nun fuer welche Zahl welche der Leuchtdioden an oder aus sein muessen. Dies ist jeweils
-;ein Pair aus einer Zahl und einer Liste der zugehoerigen Zustaende (an/aus) der Leuchtdioden. Es steht
-;#t fuer an und #f fuer aus.
-(define segmentliste '((0 . '(#t #t #t #f #t #t #t))(1 . '(#f #f #t #f #f #t #f))(2 . '(#t #f #t #t #t #f #t))(3 . '(#t #f #t #t #f #t #t))
-                       (4 . '(#f #t #t #t #f #t #f))(5 . '(#t #t #f #t #f #t #t))(6 . '(#t #t #f #t #t #t #t))(7 . '(#t #f #t #f #f #t #f))
-                       (8 . '(#t #t #t #t #t #t #t))(9 . '(#t #t #t #t #f #t #t))))
+(define (testAll)
+  (let ([liste '(2 4 3)]
+        [multi 3])
+    (values
+     (produkt1 liste multi)
+     (produkt2 liste multi)
+     (produkt3 liste multi)
+     (produkt4 liste multi)
+     (produkt5 liste multi))))
 
-;2.2
-(define (getFarbe1 zahl)
-  (if (equal? #t (caaddr(assoc zahl segmentliste)))"Red"
-      "DimGrey"))
-(define (getFarbe2 zahl)
-  (if (equal? #t (list-ref(list-ref(assoc zahl segmentliste)2)1))"Red"
-      "DimGrey"))
-(define (getFarbe3 zahl)
-  (if (equal? #t (list-ref(list-ref(assoc zahl segmentliste)2)2))"Red"
-      "DimGrey"))
-(define (getFarbe4 zahl)
-  (if (equal? #t (list-ref(list-ref(assoc zahl segmentliste)2)3))"Red"
-      "DimGrey"))
-(define (getFarbe5 zahl)
-  (if (equal? #t (list-ref(list-ref(assoc zahl segmentliste)2)4))"Red"
-      "DimGrey"))
-(define (getFarbe6 zahl)
-  (if (equal? #t (list-ref(list-ref(assoc zahl segmentliste)2)5))"Red"
-      "DimGrey"))
-(define (getFarbe7 zahl)
-  (if (equal? #t (list-ref(list-ref(assoc zahl segmentliste)2)6))"Red"
-      "DimGrey"))
+; Aufgabe 2.1
+; Die Kodierung des Zustandes könnte als Liste von Booleans in einer liste erfolgen.
+; Die Zahlen in den eckigen Klammern stehen für den Index.
+;    - [0]
+; | [1] | [2]
+;    - [3]
+; | [4] | [5]
+;    - [6]
+(define numbers
+ '(
+   (0 . (#t #t #t #f #t #t #t))
+   (1 . (#f #f #t #f #f #t #f))
+   (2 . (#t #f #t #t #t #f #t))
+   (3 . (#t #f #t #t #f #t #t))
+   (4 . (#f #t #t #t #f #t #f))
+   (5 . (#t #t #f #t #f #t #t))
+   (6 . (#t #t #f #t #t #t #t))
+   (7 . (#t #f #t #f #f #t #f))
+   (8 . (#t #t #t #t #t #t #t))
+   (9 . (#t #t #t #t #f #t #t))
+   ("off" . (#f #f #f #f #f #f #f))
+   ("-" . (#f #f #f #t #f #f #f))
+   ))
 
-(define (segmentdarstellung zahl)
-  (underlay/xy(underlay/xy(underlay/xy(underlay/xy(underlay/xy(underlay/xy (underlay/xy(rectangle 100 200 "solid" "black") 10 0 (rectangle 80 10 "solid" (getFarbe1 zahl)))
-               0 10(rectangle 10 80 "solid"(getFarbe2 zahl)))90 10(rectangle 10 80 "solid" (getFarbe3 zahl)))10 90(rectangle 80 10 "solid" (getFarbe4 zahl)))
-              0 100 (rectangle 10 80 "solid" (getFarbe5 zahl)))90 100 (rectangle 10 80 "solid" (getFarbe6 zahl)))10 180 (rectangle 80 10 "solid" (getFarbe7 zahl))))
+; Datenstruktur für ein Rechteck
+; Jedes Segment ist eine Liste mit Orientierung - Xpos - Ypos
+; Rechteckgrösse 200*100, Segmentgrösse 80*10
+; Die einzelne Segmentgrösse ist nicht exakt eingehalten
+; aus Designaspekten
+(define rectangleList
+  '(
+    (v 15 5)
+    (h 5 15)
+    (h 85 15)
+    (v 15 95)
+    (h 5 105)
+    (h 85 105)
+    (v 15 185)))
 
-;;2.3
-(define (einzelanzeige t)
-  (segmentdarstellung (scale9 t)))
 
-;;maps any number on {0-9}.
-;scales down until range is 0 - 279 (28 * 10 - 1), then divides by 28 and floors.
-(define (scale9 x)
-    (if (= x 0) 0 (floor (/ (modulo x 280) 28))))
-                
+; Aufgabe 2.2
+; Nimmt eine Nummer von 0-9 an und stellt sie als Zahl dar.
+(define (sevenSegmentNummer number)
+  (drawSevenSegmentNummer (returnStruct number) rectangleList))
 
-(define (siebensegment)
-  (animate einzelanzeige))
+; Zeichnet eine sieben Segmentanzeige mit Hilfe
+; der Liste der leuchtenden Dioden und ihrer Positionen
+(define (drawSevenSegmentNummer boolList rectangleList)
+  (if (empty? boolList)
+      (rectangle 100 200 "solid" "Black")
+      (let ([rectangleProps (car rectangleList)])
+        (underlay/xy (drawSevenSegmentNummer (cdr boolList) (cdr rectangleList)) (cadr rectangleProps) (caddr rectangleProps) (returnRectangle (car rectangleProps) (car boolList))))))
 
-;;2.4
-;1d = 2.419.200 ticks (1h * 24)
-;1h = 100.800 ticks (3600 * 28)
-;1m = 1.680 ticks (60 * 28)
-;1s = 28 ticks
-(define (6einzelanzeigen t)
-  (let* ([x (modulo t 2419200)]
-         [sekunden (modulo (modulo x 100800) 1680)]
-         [minuten (floor(/ (modulo (- x sekunden) 100800) 60))]
-         [stunden (floor(/ (- x (+ sekunden minuten)) 3600))])
-  (beside (einzelanzeige (floor (/ stunden 28)))
-          (einzelanzeige stunden)
-          (doppelpunkt)
-          (einzelanzeige (floor (/ minuten 28)))
-          (einzelanzeige minuten)
-          (doppelpunkt)
-          (einzelanzeige (floor (/ sekunden 10)))
-          (einzelanzeige sekunden))))
+; Gibt ein einzelnes Segment zurück mit der Orientierung 'v für vertikal,
+; alles andere für horizontal und einem boolean für an oder aus
+(define (returnRectangle orientation enabled)
+  (let ([color (if enabled "Red" "DimGray")])
+    (if (equal? orientation 'v)
+        (rectangle 70 10 "solid" color)
+        (rectangle 10 80 "solid" color))))
 
-(define (doppelpunkt)
-  (underlay/xy (underlay/xy (rectangle 100 200 'solid 'black)
-                            40 70 (rectangle 20 20 'solid 'red))
-               40 140 (rectangle 20 20 'solid 'red)))
+; Gibt die richtige booleanliste von der Zahlenliste zurück
+(define (returnStruct number)
+  (cdr (assoc number numbers)))
 
-(define (clock)
-  (animate 6einzelanzeigen))
+; Aufgabe 2.3
+; Animiert die Hauptaufgabe
+(define (animate7Segment)
+  (animate zeige-7segment))
+; Zeigt eine sieben Segement anzeige für eine Zahl ticks t
+; die 1 in der Funktion steht dafür, dass nur ein Sgement hier gezeigt werden soll
+(define (zeige-7segment t)
+  (zeige-x-7segment 1 (ticks->seconds t)))
+
+; Wandelt Ticks in Sekunden um.
+(define (ticks->seconds t)
+  (quotient t 28))
+
+; Aufgabe 2.4
+; Zeigt eine sieben Segmentanzeige mit x vielen Segmenten und zeigt die Zahl t
+; x sollte grösser gelich eins sein, und t wird in Base 10 dargestellt (0-9)
+(define (zeige-x-7segment x t )
+  (if (= x 1)
+      (sevenSegmentNummer (modulo t 10))
+      (beside (sevenSegmentNummer (quotient (modulo t (expt 10 x)) (expt 10 (sub1 x)))) (zeige-x-7segment (- x 1) t))))
+
+; Wie zeige-x-7segment nur ein Minus wird in einem zusätzlichen Segemnt vorangestellt
+(define (zeige-x-7segment-minus x t)
+  (beside (sevenSegmentNummer "-") (zeige-x-7segment x (abs t))))
+
+; Zeigt x-viele deaktivierte Segmente
+(define (zeige-x-7segment-off x)
+  (if (= x 1)
+      (sevenSegmentNummer "off")
+      (beside (sevenSegmentNummer "off") (zeige-x-7segment-off (- x 1)))))
+
+; Animiert die Hauptaufgabe
+(define (animateZeigeDauer)
+  (animate zeige-dauer))
+; Wandelt ticks in angezeigte Sekunden um
+; Sekunden springen bei 59 auf 00, ebenso die Minuten
+; Stunden laufen bis 99 und springen auf 00 um
+(define (zeige-dauer t)
+  (let* ([secondsRunning (ticks->seconds t)]
+         [seconds (modulo secondsRunning 60)]
+         [minutes (modulo (quotient secondsRunning 60) 60)]
+         [hours (quotient secondsRunning (* 60 60))])
+    ; Hours Segment
+    (beside (zeige-x-7segment 2 hours)
+            ; Seperator Segment
+            (returnSeperator)
+            ; Minutes Segment
+            (zeige-x-7segment 2 minutes)
+            ; Seperator Segment
+            (returnSeperator)
+            ; Seconds Segment
+            (zeige-x-7segment 2 seconds))))
+
+; Erzeugt den Seperator
+(define (returnSeperator)
+  (underlay/xy
+   (underlay/xy
+    (rectangle 100 200 'solid 'black)
+    40 70
+    (rectangle 20 20 'solid 'red))
+   40 140
+   (rectangle 20 20 'solid 'red)))
+
+; Aufgabe 2.5
+; Animiert die Hauptaufgabe
+(define (testTimer)
+  (animate (curry zeige-timer 120)))
+; Zeigt einen Timer der seconds lange läuft bei 28 ticks pro sekunde
+; Stellt 3 Segmente dar
+; Bei Ablauf der Zeit blinkt die Anzeige und es werden die abgelaufenen Sekunden
+; seit Timerende mit einem Minus davor gezeigt
+(define (zeige-timer seconds t)
+  (let ([remainingSeconds (- seconds (ticks->seconds t))]
+        [segments 3])
+    (if (< remainingSeconds 0)
+        (if (odd? (quotient t 14))
+             (zeige-x-7segment-minus (sub1 segments) remainingSeconds)
+             (zeige-x-7segment-off segments))
+        (zeige-x-7segment segments remainingSeconds))))
